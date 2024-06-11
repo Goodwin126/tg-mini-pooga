fb61de47079e51765e13aa566660204545710839
Goodwin127 <loogalas26@gmail.com>
2024-06-11 03:14:20 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/Header.vue'
+import Menu from './components/Menu.vue'
+</script>
+
+<template>
+  <div class="w-4/5 m-auto h-screen mt-10">
+    <div class="flex flex-col">
+      <Header />
+      <Menu />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+  <div class="flex justify-center">
+    <dive class="flex-col gap-2 hover:cursor-pointer">
+      <img class="transition transform hover:scale-110 translate-y-1" src="/Rectangle 1.png" />
+      <img class="mt-2 transition transform hover:scale-110 translate-y-1" src="/Rectangle 2.png" />
+      <img class="mt-2 transition transform hover:scale-110 translate-y-1" src="/Rectangle 3.png" />
+    </dive>
+    <div class="w-1/5 ml-4">
+      <h1 class="text-xl font-bold text-gray-500 text-center">Сегодня: суббота</h1>
+      <div class="flex place-content-center mt-1">
+        <img class="absolute" src="/Ellipse 1.png" />
+        <div class="mt-6 grid grid-cols-1 gap-1">
+          <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
+            <img class="w-2,5 h-2,5 m-auto mr-1" src="/Ellipse 6.png" />
+            <p class="ml-1 text-white font-bold">30% Жиры</p>
+          </div>
+          <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
+            <img class="w-2,5 h-2,5 m-auto mr-1" src="/Ellipse 7.png" />
+            <p class="flex ml-1 text-blue-900 font-bold">30% Белки</p>
+          </div>
+          <div class="flex m-auto hover:cursor-pointer transition transform hover:scale-105">
+            <img class="w-2,5 h-2,5 m-auto mr-1" src="/Ellipse 8.png" />
+            <p class="flexml-1 text-green-600 font-bold">30% Углеводы</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
