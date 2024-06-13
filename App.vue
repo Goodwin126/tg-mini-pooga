3ae476afa04348b9da49e98acfabe0f1ed463fa5
Goodwin126 <105315907+Goodwin126@users.noreply.github.com>
2024-06-14 02:38:07 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/Header.vue'
+import Menu from './components/Menu.vue'
+</script>
+
+<template>
+  <div class="flex m-auto justify-center mt-10">
+    <div class="flex flex-col">
+      <Header />
+      <Menu />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Menu.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex place-content-center mt-6">
+    <div class="flex flex-col w-5/6">
+      <div class="flex grid grid-cols-3">
+        <img class="flex-justify-left" src="/Ellipse 3.png" />
+        <div class="flex flex-col mt-3">
+          <p class="text-lg text-white">Вода 1,5 л</p>
+          <img src="/Runner_4.png" />
+          <p class="text-xs text-gray-400">рекомендовано 2,3 л</p>
+        </div>
+        <img
+          class="m-auto hover:cursor-pointer transition transform hover:scale-105"
+          src="/Button.png"
+        />
+        <img class="flex-justify-left" src="/Ellipse 4.png" />
+        <div class="flex flex-col mt-3">
+          <p class="text-lg text-white">Завтрак</p>
+          <p class="text-xs text-gray-400">рекомендовано 410 ккал</p>
+        </div>
+        <img
+          class="m-auto hover:cursor-pointer transition transform hover:scale-105"
+          src="/Button.png"
+        />
+        <img class="flex-justify-left" src="/Ellipse 2.png" />
+        <div class="flex flex-col mt-3">
+          <p class="text-lg text-white">Обед</p>
+          <p class="text-xs text-gray-400">рекомендовано 540 ккал</p>
+        </div>
+        <img
+          class="m-auto hover:cursor-pointer transition transform hover:scale-105"
+          src="/Button.png"
+        />
+        <img class="flex-justify-left" src="/Ellipse 5.png" />
+        <div class="flex flex-col mt-3">
+          <p class="text-lg text-white">Ужин</p>
+          <p class="text-xs text-gray-400">рекомендовано 320 ккал</p>
+        </div>
+        <img
+          class="m-auto hover:cursor-pointer transition transform hover:scale-105"
+          src="/Button.png"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center">
+    <div class="flex flex-center-col grid grid-cols-4 place-items-center mt-4 gap-0.5 w-5/6">
+      <img
+        class="w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
+        src="/diary.png"
+      />
+      <img
+        class="w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
+        src="/study.png"
+      />
+      <img
+        class="w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
+        src="/awards.png"
+      />
+      <img
+        class="w-14 h-14 focus:outline-none transition transform hover:scale-105 hover:-translate-y-1"
+        src="/results.png"
+      />
+      <p class="text-xs text-black hover:text-blue-800">Дневник</p>
+      <p class="text-xs text-black hover:text-blue-800">Изучить</p>
+      <p class="text-xs text-black hover:text-blue-800">Награды</p>
+      <p class="text-xs text-black hover:text-blue-800">Результаты</p>
+    </div>
+  </div>
+</template>
