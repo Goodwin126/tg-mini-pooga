00cc2064a576870d47915fc28454462266988b63
Goodwin127 <loogalas26@gmail.com>
2024-06-14 21:04:35 +0300

    secondary commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,10 @@ import Menu from './components/Menu.vue'
 </script>
 
 <template>
-  <div class="w-4/5 m-auto h-screen mt-10">
-    <div class="flex flex-col">
-      <Header />
-      <Menu />
-    </div>
+  <div class="flex flex-col">
+    <Header />
+    <Menu />
+    <h1>версии 2.0</h1>
   </div>
 </template>
 
